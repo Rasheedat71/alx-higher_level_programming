e42123e5f2638b1d20f74b8d28a25b4b05a27a43
Rasheedat Olufunke Yusuf <106766855+Rasheedat71@users.noreply.github.com>
2022-09-22 14:12:54 +0100

    file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+# !/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    a = len(sys.argv)-1
+
+    if a == 0:
+        print("{} arguments.".format(a))
+    elif a == 1:
+        print("{} argument:".format(a))
+    else:
+        print("{} arguments:".format(a))
+    for i in range(a):
+        print("{}: {}".format(a +1, sys.argv[a + 1]))
